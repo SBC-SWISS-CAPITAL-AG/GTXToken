7d37e334c42e77b7cf53470261b3e8d08856becb
Geoff <geoff@conceptpayments.com>
2018-06-27 13:41:03 +1000

    added safe math and burnFrom

--- Solidity/GTXToken.sol
+++ Solidity/GTXToken.sol
@@ -1,6 +1,56 @@
 pragma solidity 0.4.24;
 
 
+/**
+*  Open Zepplin SafeMath Library used for overflow detection.
+*
+*/
+library SafeMath {
+
+    /**
+    * @dev Multiplies two numbers, throws on overflow.
+    */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+
+    /**
+    * @dev Integer division of two numbers, truncating the quotient.
+    */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        // uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return a / b;
+    }
+
+    /**
+    * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    /**
+    * @dev Adds two numbers, throws on overflow.
+    */
+    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+
 /*
 
 BASIC ERC20 Crowdsale ICO ERC20 Token
@@ -16,6 +66,8 @@ Create this Token contract AFTER you already have the Sale contract created.
 
 
 contract BasicToken {
+    using SafeMath for uint256;
+
     uint256 public totalSupply;
     bool public allowTransfer;
 
@@ -34,8 +86,8 @@ contract StandardToken is BasicToken {
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(allowTransfer);
         require(balances[msg.sender] >= _value);
-        balances[msg.sender] -= _value;
-        balances[_to] += _value;
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
@@ -43,9 +95,9 @@ contract StandardToken is BasicToken {
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(allowTransfer);
         require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value);
-        balances[_to] += _value;
-        balances[_from] -= _value;
-        allowed[_from][msg.sender] -= _value;
+        balances[_to] = balances[_to].add(_value);
+        balances[_from] = balances[_from].sub(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
         emit Transfer(_from, _to, _value);
         return true;
     }
@@ -76,7 +128,7 @@ contract Token is StandardToken {
     string public name = "GTX Travel";
     string public symbol = "GTX";
     uint256 public decimals = 18;
-    string public version = "GTX 0.1.2";
+    string public version = "GTX 0.1.3";
     uint256 private  million = 10**6;
     mapping(address => bool) public admins;
     address constant private test_wallet1 = 0xDaa70996F99D046152477aA9f1739604e0e4011F;
@@ -94,8 +146,9 @@ contract Token is StandardToken {
 
     event adminEvent(address indexed _callingAdmin, address indexed _affectedAdmin, string action);
     event allowTransfers(address indexed _callingAdmin, bool _transfersAllowed);
+    event Burn(address indexed from, uint256 value);
 
-
+    //deny access to methods, from accounts that aren't admins.
     modifier onlyAdmins {
         require(admins[msg.sender] == true);
         _;
@@ -118,40 +171,40 @@ contract Token is StandardToken {
 
         if (testMode) {
             mintToken(test_wallet1, 200*million*(10**decimals));
-            created += balances[test_wallet1];
+            created = created.add(balances[test_wallet1]);
 
             mintToken(test_wallet2, 200*million*(10**decimals));
-            created += balances[test_wallet2];
+            created = created.add(balances[test_wallet2]);
 
             mintToken(test_wallet3, 100*million*(10**decimals));
-            created += balances[test_wallet3];
+            created = created.add(balances[test_wallet3]);
 
             mintToken(test_wallet4, 100*million*(10**decimals));
-            created += balances[test_wallet4];
+            created = created.add(balances[test_wallet4]);
 
             mintToken(test_wallet5, 75*million*(10**decimals));
-            created += balances[test_wallet5];
+            created = created.add(balances[test_wallet5]);
 
             mintToken(test_wallet6, 75*million*(10**decimals));
-            created += balances[test_wallet6];
+            created = created.add(balances[test_wallet6]);
         } else {
             mintToken(wallet1, 200*million*(10**decimals));
-            created += balances[wallet1];
+            created = created.add(balances[wallet1]);
 
             mintToken(wallet2, 200*million*(10**decimals));
-            created += balances[wallet2];
+            created = created.add(balances[wallet2]);
 
             mintToken(wallet3, 100*million*(10**decimals));
-            created += balances[wallet3];
+            created = created.add(balances[wallet3]);
 
             mintToken(wallet4, 100*million*(10**decimals));
-            created += balances[wallet4];
+            created = created.add(balances[wallet4]);
 
             mintToken(wallet5, 75*million*(10**decimals));
-            created += balances[wallet5];
+            created = created.add(balances[wallet5]);
 
             mintToken(wallet6, 75*million*(10**decimals));
-            created += balances[wallet6];
+            created = created.add(balances[wallet6]);
         }
 
         require(created == initial_expected_total);
@@ -165,9 +218,9 @@ contract Token is StandardToken {
         require(allowTransfer);
         require(to != address(0)); //check an address was entered.
         require((amount > 0) && (amount < (250*million*(10**decimals))));
-        require(totalSupply <= (totalSupply + amount)); //overflow check
-        balances[to] += amount;
-        totalSupply += amount;
+        require(totalSupply <= (totalSupply.add(amount))); //overflow check
+        balances[to] = balances[to].add(amount);
+        totalSupply = totalSupply.add(amount);
         emit Transfer(address(0), to, amount);
         return true;
     }
@@ -193,6 +246,22 @@ contract Token is StandardToken {
         admins[oldAdmin] = false;
         emit adminEvent(msg.sender, oldAdmin, "removed");
     }
+
+    /**
+    * allows an admin to delete / burn tokens from an account.
+    * reduces the total supply of tokens in circulation
+    *
+    * @param _from the address of the account to burn the tokens from
+    * @param _value the amount of tokens to burn
+    */
+    function burnFrom(address _from, uint256 _value) onlyAdmins public returns (bool success) {
+        require(balances[_from] >= _value);                 // Check if the targeted balance is enough
+        balances[_from] = balances[_from].sub(_value);                          // Subtract from the targeted balance
+        totalSupply = totalSupply.sub(_value);                              // Update totalSupply
+        emit Burn(_from, _value);
+        return true;
+    }
+
 
     /**
     * Set allowance for other address and notify
